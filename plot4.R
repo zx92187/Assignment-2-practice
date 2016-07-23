d1c91bf4b6a3b2837ded39f08ef1057a955e8c7c
zx92187 <mzhen1988@gmail.com>
2016-07-22 20:12:27 -0600

    Add files via upload

---
+++ plot4.R
@@ -0,0 +1,31 @@
+
+#######################################################
+# Exploratory Data Analysis - Proeject 2 - Question #4#
+#######################################################
+
+library(ggplot2)
+
+# Loading provided datasets - loading from my pre-set working directory by using the "setwd" commend
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+
+# Coal
+SCC.coal = SCC[grepl("coal", SCC$Short.Name, ignore.case=TRUE),]
+
+# Merging two data sets
+merge <- merge(x=NEI, y=SCC.coal, by='SCC')
+merge.sum <- aggregate(merge[, 'Emissions'], by=list(merge$year), sum)
+colnames(merge.sum) <- c('Year', 'Emissions')
+
+
+png(filename='plot4.png')
+
+ggplot(data=merge.sum, aes(x=Year, y=Emissions/1000)) + 
+  geom_line(aes(group=1, col=Emissions)) + geom_point(aes(size=2, col=Emissions)) + 
+  ggtitle(expression('Total Emissions of PM'[2.5])) + 
+  ylab(expression(paste('PM', ''[2.5], ' in kilotons'))) + 
+  geom_text(aes(label=round(Emissions/1000,digits=2), size=2, hjust=1.5, vjust=1.5)) + 
+  theme(legend.position='none') + scale_colour_gradient(low='blue', high='red')
+
+dev.off()
