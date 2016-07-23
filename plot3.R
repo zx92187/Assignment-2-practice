5a5972cdda681960fa34d88e7f905b53f8970ea8
zx92187 <mzhen1988@gmail.com>
2016-07-22 20:08:46 -0600

    Add files via upload

---
+++ plot3.R
@@ -0,0 +1,30 @@
+
+
+#######################################################
+# Exploratory Data Analysis - Proeject 2 - Question #3#
+#######################################################
+
+library(ggplot2)
+
+# Loading provided datasets - loading from my pre-set working directory by using the "setwd" commend
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# Sampling
+NEI_sampling <- NEI[sample(nrow(NEI), size=5000, replace=F), ]
+
+# Baltimore City == fips: 24510
+MD <- subset(NEI, fips == 24510)
+MD$year <- factor(MD$year, levels=c('1999', '2002', '2005', '2008'))
+
+
+# Generate the graph in the same directory as the source code
+png(filename='plot3.png', width=800, height=500, units='px')
+
+ggplot(data=MD, aes(x=year, y=log(Emissions))) + facet_grid(. ~ type) + guides(fill=F) +
+  geom_boxplot(aes(fill=type)) + stat_boxplot(geom ='errorbar') +
+  ylab(expression(paste('Log', ' of PM'[2.5], ' Emissions'))) + xlab('Years') + 
+  ggtitle('Emissions per Type in Baltimore') +
+  geom_jitter(alpha=0.10)
+
+dev.off()
