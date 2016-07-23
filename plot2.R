d239e8bf585e30ca9b76fff13cd20d020ab5a891
zx92187 <mzhen1988@gmail.com>
2016-07-22 20:04:26 -0600

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,25 @@
+setwd("C:/Users/MIAO/Desktop/R directory")
+
+#######################################################
+# Exploratory Data Analysis - Proeject 2 - Question #1#
+#######################################################
+
+# Loading provided datasets - loading from my pre-set working directory by using the "setwd" commend
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# Sampling
+NEI_sampling <- NEI[sample(nrow(NEI), size=5000, replace=F), ]
+
+# Subset data and append two years in one data frame
+MD <- subset(NEI, fips=='24510')
+
+
+# Generate the graph in the same directory as the source code
+png(filename='plot2.png')
+
+barplot(tapply(X=MD$Emissions, INDEX=MD$year, FUN=sum), 
+        main='Total Emissions in Baltimore City, MD', 
+        xlab='Years', ylab=expression('PM'[2.5]))
+
+dev.off()
