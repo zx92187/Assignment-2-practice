2e328b29d727cbe72b0118ff79db8cafceac5fc3
zx92187 <mzhen1988@gmail.com>
2016-07-22 20:37:19 -0600

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -13,12 +13,12 @@ SCC <- readRDS("Source_Classification_Code.rds")
 # Sampling
 NEI_sampling <- NEI[sample(nrow(NEI), size=5000, replace=F), ]
 
-# Baltimore City == fips: 24510
+# Baltimore City, fips = 24510
 MD <- subset(NEI, fips == 24510)
 MD$year <- factor(MD$year, levels=c('1999', '2002', '2005', '2008'))
 
 
-# Generate the graph in the same directory as the source code
+# Graphing
 png(filename='plot3.png', width=800, height=500, units='px')
 
 ggplot(data=MD, aes(x=year, y=log(Emissions))) + facet_grid(. ~ type) + guides(fill=F) +
